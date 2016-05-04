bc1b9d5cd59133eaa2f81439f96f597be021ff08
Kevin Piacentini <kevinpiac@gmail.com>
2016-05-04 14:31:01 +0200

    Add date options and tests

--- dst/src/sort.c
+++ dst/src/sort.c
@@ -12,16 +12,6 @@
 
 #include "ft_ls.h"
 
-void 		sort(t_vector *v, t_opm_params *opm)
-{
-	if (opm_issetoption(opm->config, "r"))
-		ft_putendl("have to sort with option r");
-	else if (opm_issetoption(opm->config, "t"))
-		ft_putendl("have to sort with option t");
-	else
-		sort_lexico(v);
-}
-
 void		sort_lexico(t_vector *v)
 {
 	int			i;
@@ -43,4 +33,60 @@ void		sort_lexico(t_vector *v)
 		}
 		i++;
 	}
+}
+
+void		sort_lexico_inverted(t_vector *v)
+{
+	int			i;
+	t_filedata	*tmp;
+	t_filedata	*file;
+	t_filedata	*next;
+
+	i = 0;
+	while (i < v->total - 1)
+	{
+		file = ft_vectget(v, i);
+		next = ft_vectget(v, i + 1);
+		if (ft_strcmp(file->filename, next->filename) < 0)
+		{
+			tmp = v->items[i];
+			v->items[i] = v->items[i + 1];
+			v->items[i + 1] = tmp;
+			i = -1;
+		}
+		i++;
+	}
+}
+
+static void sort_date(t_vector *v)
+{
+	int			i;
+	t_filedata	*tmp;
+	t_filedata	*file;
+	t_filedata	*next;
+
+	i = 0;
+	while (i < v->total - 1)
+	{
+		file = ft_vectget(v, i);
+		next = ft_vectget(v, i + 1);
+		if (file->stat->st_mtime < next->stat->st_mtime)
+		{
+			tmp = v->items[i];
+			v->items[i] = v->items[i + 1];
+			v->items[i + 1] = tmp;
+			i = -1;
+		}
+		i++;
+	}
+}
+
+void 		sort(t_vector *v, t_opm_params *opm)
+{
+	if (opm_issetoption(opm->config, "r"))
+		sort_lexico_inverted(v);
+	else if (opm_issetoption(opm->config, "t"))
+		sort_date(v);
+	else
+		sort_lexico(v);
 }
